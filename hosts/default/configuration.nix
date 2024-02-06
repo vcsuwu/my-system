0c8ce74896a231af565011dd69b1a96f0bdbd9f5
vocus <zenyabtw@gmail.com>
2024-02-06 18:11:33 +0500

    initial commit

---
+++ hosts/default/configuration.nix
@@ -0,0 +1,75 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+  [
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.default
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  networking.hostName = "linux";
+  networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.networkmanager.enable = true;
+  time.timeZone = "Asia/Yekaterinburg";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+	  enable = true;
+	  alsa.enable = true;
+	  alsa.support32Bit = true;
+	  pulse.enable = true;
+  };
+
+  security.polkit.enable = true;
+
+  fonts.packages = with pkgs; [
+	  noto-fonts
+	  noto-fonts-cjk
+	  noto-fonts-emoji
+	  (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
+
+  users.users.vocus = {
+    isNormalUser = true;
+    description = "vocus";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    useGlobalPkgs = true;
+    users = {
+      "vocus" = import ./home.nix;
+    };
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+     neovim
+     git
+  ];
+
+  system.stateVersion = "23.11";
+}
---
+++ hosts/default/home.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "vocus";
+  home.homeDirectory = "/home/vocus";
+  home.packages = with pkgs; [
+    neofetch
+    nnn
+
+    zip
+    xz
+    unzip
+    p7zip
+
+    brave
+    rofi-wayland
+    foot
+    grim
+    slurp
+    wl-clipboard
+    cliphist
+    vesktop
+    keepassxc
+    telegram-desktop
+    vscode
+    hyprpaper
+    xdg-desktop-portal-hyprland
+    xdg-desktop-portal-gtk
+    zathura
+  ];
+
+  programs = {
+    git = {
+    enable = true;
+    userName = "vocus";
+    userEmail = "zenyabtw@gmail.com";
+    };
+
+    direnv = {
+      enable = true;
+      enableBashIntegration = true; # see note on other shells below
+    };
+
+    bash = {
+      enable = true;
+    };
+
+    home-manager.enable = true;
+  };
+
+  home.stateVersion = "23.11";
+}
